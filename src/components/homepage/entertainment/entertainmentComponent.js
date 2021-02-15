1d2c5a4cfe216bae1b63c8295d122c81b2f9f108
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2021-02-15 16:42:09 +0100

    added completely styled entertainment component

--- src/components/homepage/entertainment/entertainmentComponent.js
+++ src/components/homepage/entertainment/entertainmentComponent.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Image1 from "../../../assets/images/tech_1.jpg";
-import Image2 from "../../../assets/images/athletic.jpg";
-import Image3 from "../../../assets/images/tech2.jpg";
-import Image4 from "../../../assets/images/travel1.jpg";
-import { Card, Button } from "react-bootstrap";
+import Rmd from "../../../assets/images/rmd.jpg";
+import Drake from "../../../assets/images/drake.jpg";
+import Marlian from "../../../assets/images/marlian.jpg";
+import Bboy from "../../../assets/images/burnaboy.jpg";
+import { Card } from "react-bootstrap";
 import "./entertainment.css";
 
 const lifestyle = () => {
@@ -14,46 +14,60 @@ const lifestyle = () => {
         <div className="container-fluid row w-100 bg-white mx-auto ent-pos-tp">
           <div className="col-sm-3 my-2 mg-sm-scr">
             <Card className="bg-dark text-white mt-wrap">
-              <Card.Img src={Image1} alt="Card image" className="ent-h-100" />
+              <Card.Img src={Rmd} alt="Card image" className="ent-h-100" />
               <Card.ImgOverlay className="overl">
                 <Card.Text className="">
                   RMD TALKS <br /> ABOUT AGING
                 </Card.Text>
               </Card.ImgOverlay>
-            <a href="http://facebook.com"> RMD: Biography of an Iconic Nigerian actor Richard</a>
+              <a href="http://facebook.com">
+                {" "}
+                RMD – Biography Of An Iconic Nigerian Actor, Richard Mofe-Damijo
+              </a>
             </Card>
           </div>
           <div className="col-sm-3 my-2 mg-sm-scr">
             <Card className="bg-dark text-white mt-wrap">
-            <Card.Img src={Image1} alt="Card image" className="ent-h-100" />
+              <Card.Img src={Drake} alt="Card image" className="ent-h-100" />
               <Card.ImgOverlay className="overl">
                 <Card.Text className="">
                   DRAKE TALKS <br /> BUSINESS
                 </Card.Text>
               </Card.ImgOverlay>
-            <a href="http://facebook.com"> Drake Praises young Nigerian artist who went viral...</a>
+              <a href="http://facebook.com">
+                {" "}
+                Drake Praises Young Nigerian Artist Hyzah Who Went Viral: 'U Are
+                Amazing'
+              </a>
             </Card>
           </div>
           <div className="col-sm-3 my-2 mg-sm-scr">
-          <Card className="bg-dark text-white mt-wrap">
-            <Card.Img src={Image1} alt="Card image" className="ent-h-100" />
+            <Card className="bg-dark text-white mt-wrap">
+              <Card.Img src={Marlian} alt="Card image" className="ent-h-100" />
               <Card.ImgOverlay className="overl">
-                <Card.Text className="">
-                  NAIRA MARLEY
-                </Card.Text>
+                <Card.Text className="">NAIRA MARLEY</Card.Text>
               </Card.ImgOverlay>
-            <a href="http://facebook.com"> Drake Praises young Nigerian artist who went viral...</a>
+              <a href="http://facebook.com">
+                {" "}
+                Azeez Adeshina Fashola (born 10 May 1991), known professionally
+                as Naira Marley, is a Nigerian singer and songwriter.
+              </a>
             </Card>
           </div>
           <div className="col-sm-3 my-2 mg-sm-scr">
-          <Card className="bg-dark text-white mt-wrap">
-            <Card.Img src={Image1} alt="Card image" className="ent-h-100" />
+            <Card className="bg-dark text-white mt-wrap">
+              <Card.Img src={Bboy} alt="Card image" className="ent-h-100" />
               <Card.ImgOverlay className="overl">
                 <Card.Text className="">
-                  BURNA BOY
+                  BURNA <br /> BOY
                 </Card.Text>
               </Card.ImgOverlay>
-            <a href="http://facebook.com"> Drake Praises young Nigerian artist who went viral...</a>
+              <a href="http://facebook.com">
+                {" "}
+                Damini Ebunoluwa Ogulu (born 2 July 1991), known professionally
+                as Burna Boy, is a Nigerian singer, songwriter, rapper and
+                dancer.{" "}
+              </a>
             </Card>
           </div>
         </div>
